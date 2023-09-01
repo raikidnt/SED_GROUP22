a1d8dae2eacca753e8e4c3ca52eef033ef327228
Kevzinn <kqnguyen2036@gmail.com>
2023-09-01 18:29:57 +0700

    Update Bike.h

--- .vscode/Class/Bike/Bike.h
+++ .vscode/Class/Bike/Bike.h
@@ -24,7 +24,9 @@ class Bike {
       Bike(std::string i_model,std::string i_color,int i_engineSize, int i_year, int cost){}
 
       void showInfo(){}
-      void list(){}     //to list available models
+      void list(int choice){ //to list available models
+         
+      }     
       void unlist(){}   // to unlist unwanted bike
 };
 // #endif   // BIKE_H
