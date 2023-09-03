03571d256794ee1264e068cb3fcc9a79f145668d
raikidnt <118874686+raikidnt@users.noreply.github.com>
2023-09-03 18:11:31 +0700

    update user classes

---
+++ .vscode/Class/User/User.h
@@ -0,0 +1,14 @@
+#ifndef USER_H
+#define USER_H
+#include <bits/stdc++.h>
+using namespace std;
+
+class User{
+    public:
+    string userName;
+    string password;
+    string phonenumber;
+    
+};
+
+#endif
