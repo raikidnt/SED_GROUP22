7e4b8e3e04da9113718b61155dbb890272ec2c48
Kevzinn <kqnguyen2036@gmail.com>
2023-09-10 20:53:51 +0700

    d

--- class.h
+++ class.h
@@ -2,20 +2,20 @@
 #define CLASS
 #include <string>
 
-class User {
-   private:
-      std::string username;
-      std::string password;
-      std::string fullname;
-      int userID;    //user ID: auto assigned when register as a user 
-   public:
-};
+// class User {
+//    private:
+//       std::string username;
+//       std::string password;
+//       std::string fullname;
+//       int userID;    //user ID: auto assigned when register as a user 
+//    public:
+// };
 
-class Motobike {
-   private:
-      int rating = 3;
-   public:
-      int get_rating();
-};
+// class Motobike {
+//    private:
+//       int rating = 3;
+//    public:
+//       int get_rating();
+// };
 
 #endif   //CLASS
