6bed1c6da91985159df342f080a366f546b59b71
minhquan <s3938194@rmit.edu.vn>
2023-09-19 01:57:03 +0700

    x
    x

--- main.cpp
+++ main.cpp
@@ -1,16 +1,50 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+#include <iostream>
+#include <fstream>
+#include ".vscode\Class\User\Member.cpp"
+#include ".vscode\Class\UI\UI.cpp"
+#include ".vscode\Class\Bike\Motorbike.cpp"
+=======
+>>>>>>> Stashed changes
 #include <bits/stdc++.h>
 // #include ".vscode\Class\User\Member.cpp"
 // #include ".vscode\Class\UI\UI.cpp"
+<<<<<<< Updated upstream
 // #include ".vscode/Class/User/Admin.cpp"   
 // #include ".vscode/Class/User/User.cpp"   
 #include ".vscode/Class/UI/UI.cpp"
+=======
+#include ".vscode/Class/User/Admin.cpp"   
+#include ".vscode/Class/User/User.cpp"   
+#include <string>
+>>>>>>> c0f1d76a0c0ee6fb3f36a908d7c87f63bd61eff8
+=======
+// #include <bits/stdc++.h>
+// #include ".vscode\Class\User\Member.cpp"
+// #include ".vscode\Class\UI\UI.cpp"
+// #include ".vscode/Class/User/Admin.cpp"   
+// #include ".vscode/Class/User/User.cpp"
+
+#include ".vscode/Class/UI/UI.cpp"
+
+#define memberFile ".vscode/Data/Member.txt"
+// #include <string>
+>>>>>>> d56694d5d12ad1911814cb6e55bb48e764b55d5f
+>>>>>>> Stashed changes
 using namespace std;
 
 int main(int argc, char *argv[]) {
    // welcome();
    // MotorBike mk1 ("Futures Neo", "Black", manual,  125,  2022, 6);
-   // Member mb1("Nhat Nguyen", 865608476, CCCD , 00112233,"9959334",  "30/12/2023", 10);
+
+   // Member mb1("Nhat Nguyen", "0865608476", 
+   //             "CCCD" , "00112233",12,  
+   //             "22312","30/12/2023", 2.3);
    // mb1.showInfo_M();
+
    // mb1.addBike(mk1);
    // mb1.showInfo_M();
    
@@ -23,6 +57,23 @@ int main(int argc, char *argv[]) {
    // sys.showMembersList();
    sys.adminViewMembers();
 
+<<<<<<< Updated upstream
+=======
+   // Admin ad;
+   // string user;
+   // string pass;
+   // Admin ad;
+   // ad.login(user,pass);
+   
+   System sys;
+
+
+   // sys.loadBikes();
+   
+   sys.loadMembers();
+   sys.showMembersList();
+   
+>>>>>>> Stashed changes
 
    return 0;
 }
